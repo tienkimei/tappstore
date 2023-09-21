69a1ca9d088702e64a15282c36ea23a6ca970b4e
unknown <tienlnt@kimei.vn>
2023-09-21 09:12:17 +0700

    tien tien

--- appstore/src/pages/product/Products.jsx
+++ appstore/src/pages/product/Products.jsx
@@ -5,6 +5,7 @@ import Carts from './Carts'
 export default function Products(props) {
   return (
     <div className='container'>
+      <h3>hello</h3>
       <Carts/>
       <div className='row'>
         <div className='col-3'>
